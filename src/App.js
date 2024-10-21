ff5ef5d8160f04438a97cc99e95f1063537c763d
ameliasusianti-pasti <147461013+ameliasusianti-pasti@users.noreply.github.com>
2024-10-21 19:51:54 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>{0}</h1>
+        <button onClick={() => console.log('test')}>Increment</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
